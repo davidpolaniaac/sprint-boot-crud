fd9fd781acf61b66d902cb44d7725727001ac3fe
David Polania <davidpolaniaac@users.noreply.github.com>
2023-01-26 14:27:28 -0500

    add model and use case

---
+++ domain/model/src/main/java/co/com/crudtest/model/product/gateways/ProductRepository.java
@@ -0,0 +1,14 @@
+package co.com.crudtest.model.product.gateways;
+
+import java.util.List;
+
+import co.com.crudtest.model.product.Product;
+
+public interface ProductRepository {
+
+    void create(Product product);
+    Product read(String id);
+    void update(String id, Product product);
+    void delete(String id);
+    List<Product> getAll();
+}
---
+++ domain/usecase/src/main/java/co/com/crudtest/usecase/crudproduct/CrudProductUseCase.java
@@ -0,0 +1,6 @@
+package co.com.crudtest.usecase.crudproduct;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class CrudProductUseCase {
+}
